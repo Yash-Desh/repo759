214dfcc11d253c57450ddff03c813734f4170d59
Yash Deshpande <23yashd@gmail.com>
2024-09-14 16:06:36 -0500

    spaces removed

--- HW01/task4.sh
+++ HW01/task4.sh
@@ -5,7 +5,5 @@
 #SBATCH -e FirstSlurm-%j.err
 #SBATCH -c 2
 #SBATCH -t 0-00:01:00
-
 cd $SLURM_SUBMIT_DIR
-
 hostname
--- HW01/task6.sh
+++ HW01/task6.sh
@@ -5,9 +5,7 @@
 #SBATCH -e FirstSlurm-%j.err
 #SBATCH -c 1
 #SBATCH -t 0-00:01:00
-
 cd $SLURM_SUBMIT_DIR
-
 module load gcc/11.3.0
 cd repo759/HW01
 g++ task6.cpp -Wall -O3 -std=c++17 -o task6
