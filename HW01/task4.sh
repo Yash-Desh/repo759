ca94f10b66ec71c6ebc9c7ff310ef9b58a826cdd
Yash Deshpande <23yashd@gmail.com>
2024-09-14 15:53:21 -0500

    Updated shell files

--- HW01/task4.sh
+++ HW01/task4.sh
@@ -2,15 +2,15 @@
 
 #SBATCH –p instruction
 
-#SBATCH --job-name=FirstSlurm
+#SBATCH -J FirstSlurm
 
-#SBATCH -o FirstSlurm-%j.txt
+#SBATCH -o FirstSlurm-%j.out
 
 #SBATCH -e FirstSlurm-%j.err
 
 #SBATCH -c 2
 
-#SBATCH -t 0-00:30:00
+#SBATCH -t 0-00:01:00
 
 cd $SLURM_SUBMIT_DIR
 
--- HW01/task6.sh
+++ HW01/task6.sh
@@ -2,18 +2,19 @@
 
 #SBATCH –p instruction
 
-#SBATCH --job-name=FirstSlurm
+#SBATCH -J FirstSlurm
 
-#SBATCH -o FirstSlurm-%j.txt
+#SBATCH -o FirstSlurm-%j.out
 
 #SBATCH -e FirstSlurm-%j.err
 
 #SBATCH -c 1
 
-#SBATCH -t 0-00:30:00
+#SBATCH -t 0-00:01:00
 
 cd $SLURM_SUBMIT_DIR
 
 module load gcc/11.3.0
+cd repo759/HW01
 g++ task6.cpp -Wall -O3 -std=c++17 -o task6
 ./task6 12
