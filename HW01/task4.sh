c0238f4f0bbecaf22c3810d83cac1abee36906ac
Yash Deshpande <23yashd@gmail.com>
2024-09-14 16:00:25 -0500

    Spaces Removed from shell files

--- HW01/task4.sh
+++ HW01/task4.sh
@@ -1,15 +1,9 @@
 #!/usr/bin/env zsh
-
 #SBATCH –p instruction
-
 #SBATCH -J FirstSlurm
-
 #SBATCH -o FirstSlurm-%j.out
-
 #SBATCH -e FirstSlurm-%j.err
-
 #SBATCH -c 2
-
 #SBATCH -t 0-00:01:00
 
 cd $SLURM_SUBMIT_DIR
