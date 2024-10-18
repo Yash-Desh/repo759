b68b37afe8c3c6250d44712b579c53f58a2b5e62
Yash Deshpande <23yashd@gmail.com>
2024-10-18 13:58:51 -0500

    modified convolution.cpp and task2.cpp to improve parallelization

--- HW03/convolution.cpp
+++ HW03/convolution.cpp
@@ -4,67 +4,19 @@ using namespace std;
 
 void convolve(const float *image, float *output, std::size_t n, const float *mask, std::size_t m)
 {
-    unsigned int x = 0;
-    unsigned int y = 0;
-
-    #pragma omp parallel for 
-    for (unsigned int k = 0; k < n * n; k++)
+    #pragma omp for collapse(3)
+    for (size_t x = 0; x < n; x++)
     {
-        if (y == n)
+        for (size_t y = 0; y < n; y++)
         {
-            y = 0;
-            x++;
+            for (size_t i = 0; i < m; i++)
+            {
+                for (size_t j = 0; j < m; j++)
+                {
+                    #pragma omp atomic
+                    output[x * n + y] += mask[i * m + j] * image[(x + i) * (n + 2) + (y + j)];
+                }
+            }
         }
-
-        // loop to calculate element value
-        float sum = 0;
-        unsigned int i = 0, j = 0;
-        for (unsigned int l = 0; l < m * m; l++)
-        {
-            if (j == m)
-            {
-                j = 0;
-                i++;
-            }
-            float op = 0;
-            unsigned int x_index = x + i - (m - 1) / 2;
-            unsigned int y_index = y + j - (m - 1) / 2;
-
-            // cout<<i*m+j<<"  : x_index = "<<x_index<<" , y_index = "<<y_index<<endl;
-
-            // determine the op
-            if (!(x_index >= 0 && x_index < n) && !(y_index >= 0 && y_index < n))
-            {
-                op = 0;
-            }
-            else if ((((x_index >= 0) && (x_index < n)) && ((y_index < 0) || (y_index >= n))) ||
-                     (((x_index < 0) || (x_index >= n)) && ((y_index >= 0) && (y_index < n))))
-            {
-                // cout<<"x_index evaluator ="<<((x_index >= 0) && (x_index < n))<<endl;
-                // cout<<"y_index evaluator ="<< ((y_index >= 0) && (y_index < n))<<endl;
-                // cout<<"else if entered"<<endl;
-                op = 1;
-            }
-            else
-            {
-                op = image[x_index * n + y_index];
-            }
-            // cout<<"op = "<<op<<" ";
-            // cout<<"mask value = "<<mask[i * m + j]<<endl;
-            #pragma omp atomic
-            sum += (op * mask[i * m + j]);
-            j++;
-        }
-        // cout<<endl;
-        output[x * n + y] = sum;
-        y++;
     }
-
-    // loop to print the output array
-    // for (unsigned int k = 0; k < n * n; k++)
-    // {
-    //     cout << output[k] << " ";
-    // }
-    // cout << endl;
-    
 }
--- HW03/task2_1.cpp
+++ HW03/task2_1.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
-#include <random>                   // To generate random numbers
-#include <cstring>                  // To use memset
+#include <random>  // To generate random numbers
 #include "convolution.h"
 
 // The std::chrono namespace provides timer functions in C++
@@ -19,47 +18,31 @@ int main(int argc, char* argv[])
     high_resolution_clock::time_point end;
     duration<double, std::milli> duration_sec;
 
-    // n is dimension of image
+    // n & t are given as command line arguments
     size_t n = atoi(argv[1]);
 
     // t is the number of threads
     size_t t = atoi(argv[2]);
 
-    // size of the mask is 3
+    // size of the mask is fixed at 3
     size_t m = 3;
 
-    // image array of dimension n
+    // array to store n*n random float values from -10.0 to 10.0 for image
     float *image_arr = new float[n*n];
     if (!image_arr)
     {
         cout << "Memory allocation failed for array of random floats\n";
     }
 
-    // array to store extended image
-    float *image_mod = new float[(n+2)*(n+2)];
-    if (!image_mod)
-    {
-        cout << "Memory allocation failed for extended image array\n";
-    }
-
-    // set all elements in image_mod to 0
-    memset(image_mod, 0, (n+2) * (n+2) * sizeof(image_mod[0]));
-
-
-    // mask array of dimension m
+    // array to store the m*m random float values from -1.0 to 1.0 for mask
     float *mask_arr = new float[m*m];
     if (!mask_arr)
     {
-        cout << "Memory allocation failed for mask array\n";
+        cout << "Memory allocation failed for scan array of random floats\n";
     }
 
-    // output of the convolution of dimension n
+    // array to store n*n float values for output
     float *output = new float[n*n];
-    memset(output, 0, n * n * sizeof(output[0]));
-    if (!output)
-    {
-        cout << "Memory allocation failed for output array\n";
-    }
 
     std::random_device entropy_source;
 
@@ -69,32 +52,11 @@ int main(int argc, char* argv[])
     std::uniform_real_distribution<float> dist1(-10.0, 10.0);
 	std::uniform_real_distribution<float> dist2(-1.0, 1.0);
 
-	// Populate the image array
+	// Write a random value to each slot in N
 	for (size_t i = 0; i < n*n; i++) {
 		image_arr[i] = dist1(generator);
 	}
 
-    // for loops to create an extended array 
-    for(size_t i=0; i<(n+2); i++)
-    {
-        for(size_t j=0; j<(n+2); j++)
-        {
-            if(((i == 0 || i == n+1) && (j>=1 && j <= n)) || ((j==0 || j== n+1) && (i>=1 && i<=n)))
-            {
-                image_mod[i*(n+2) + j]=1;
-            }
-            else if((i==0 || i==n+1) && (j==0 || j==n+1))
-            {
-                continue;
-            }
-            else
-            {
-                image_mod[i*(n+2) + j] = image_arr[(i-1)*n + (j-1)];
-            }
-        }
-    }
-
-    // populate the mask array
     for (size_t i = 0; i < m*m; i++) {
 		mask_arr[i] = dist2(generator);
 	}
@@ -104,7 +66,7 @@ int main(int argc, char* argv[])
     omp_set_num_threads(t);
     #pragma omp parallel
     {
-        convolve(image_mod, output, n, mask_arr, m);
+        convolve(image_arr, output, n, mask_arr, m);
     }
     end = high_resolution_clock::now();
     // Convert the calculated duration to a double using the standard library
@@ -119,7 +81,7 @@ int main(int argc, char* argv[])
 
     // for(size_t i = 0; i < n*n; i++)
     //     cout<<image_arr[i]<<" ";
-    // cout<<endl<<endl<<endl;
+    // cout<<endl;
 
     // for(size_t i = 0; i < m*m; i++)
     //     cout<<mask_arr[i]<<" ";
@@ -131,7 +93,6 @@ int main(int argc, char* argv[])
 
     // deallocate memory
     delete[] image_arr;
-    delete[] image_mod;
     delete[] mask_arr;
     delete[] output;
 
