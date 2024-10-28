a229333bc6bcbe229d5c411f1612d9abefcafecd
Yash Deshpande <23yashd@gmail.com>
2024-10-27 21:03:46 -0500

    Initial commit for HW04

---
+++ HW04/task3_1.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env zsh
+
+#SBATCH -p instruction
+
+#SBATCH -J Task4
+
+#SBATCH -o Task4-%j.out
+
+#SBATCH -e Task4-%j.err
+
+#SBATCH --cpus-per-task=8
+
+#SBATCH -t 0-00:01:00
+
+cd $SLURM_SUBMIT_DIR
+
+module load gcc/11.3.0
+cd repo759/HW03
+g++ task3.cpp -Wall -O3 -std=c++17 -o task3 -fopenmp
+
+export OMP_SCHEDULE="static,1"
+
+for ((counter=1; counter<=8; counter = counter+1))
+do
+./task3 100 10 $counter
+printf "\n"
+done
