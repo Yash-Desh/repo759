cc85f132f2b8e80a11a4cb636822edc191ecc6b7
Yash Deshpande <23yashd@gmail.com>
2024-11-08 12:22:42 -0600

    task3 files added

--- HW05/task3.cu
+++ HW05/task3.cu
@@ -19,10 +19,6 @@ int main(int argc, char* argv[])
     high_resolution_clock::time_point end;
     duration<double, std::milli> duration_sec;
 
-    // n is given as command line argument
-    unsigned int n = atoi(argv[1]);
-    
-
     std::random_device entropy_source;
 
 	// 64-bit variant of mt19937 isn't necessary here, but it's just an example
@@ -31,15 +27,63 @@ int main(int argc, char* argv[])
     std::uniform_real_distribution<float> dist1(-10.0, 10.0);
 	std::uniform_real_distribution<float> dist2(0.0, 1.0);
 
+    
+    // n is given as command line argument
+    unsigned int n = atoi(argv[1]);
+    
+
+    // array to store n*n random float values from -10.0 to 10.0 for image
+    float *a = new float[n];
+    if (!a)
+    {
+        cout << "Memory allocation failed for array a\n";
+    }
+
+    // array to store the n*n random float values from 0.0 to 1.0 for mask
+    float *b = new float[n];
+    if (!b)
+    {
+        cout << "Memory allocation failed for array b\n";
+    }
+
+    // Write a random value to each slot in N
+	for (size_t i = 0; i < n; i++) {
+		a[i] = dist1(generator);
+	}
+
+    for (size_t i = 0; i < n; i++) {
+		b[i] = dist2(generator);
+	}
+
+    float *d_a, *d_b;
+    int size = n * sizeof(float);
+
+    //allocate memory on the device (GPU)   
+    cudaMalloc((void**)&d_a, size);
+    cudaMalloc((void**)&d_b, size);
+
+    cudaMemcpy(d_a, a, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_b, b, size, cudaMemcpyHostToDevice);
+
+    // threads per block
+    int M=512;
+    
+    
     // Get the starting timestamp
     start = high_resolution_clock::now();
-    vscale<<<(N + M-1) / M,M>>>(d_a, d_b, d_c, N);
+    vscale<<<(n + M-1) / M,M>>>(a, b, n);
     end = high_resolution_clock::now();
     // Convert the calculated duration to a double using the standard library
     duration_sec = std::chrono::duration_cast<duration<double, std::milli>>(end - start);
 
+    cudaMemcpy(b, d_b, size, cudaMemcpyDeviceToHost);
+
     // Durations are converted to milliseconds already thanks to std::chrono::duration_cast
     cout << duration_sec.count() << endl;
-    cout << output[0] << endl;
-    cout << output[n*n - 1] << endl;
+    cout << b[0] << endl;
+    cout << b[n - 1] << endl;
+
+    // deallocate memory
+    delete[] a;
+    delete[] b;
 }
---
+++ HW05/task3.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env zsh
+
+#SBATCH -p instruction
+
+#SBATCH -J Task3
+
+#SBATCH -o Task3-%j.out
+
+#SBATCH -e Task3-%j.err
+
+#SBATCH -c 1
+
+#SBATCH --ntasks=1
+
+#SBATCH --gpus-per-task=1
+
+#SBATCH -t 0-00:01:00
+
+cd $SLURM_SUBMIT_DIR
+
+
+module load nvidia/cuda/11.8.0
+module load gcc/11.3.0
+
+nvcc task3.cu vscale.cu -Xcompiler -O3 -Xcompiler -Wall -Xptxas -O3 -std=c++17 -o task3
+
+
+for ((counter=2**10; counter<=2**29; counter = counter*2))
+do
+./task3 counter
+printf "\n"
+done
