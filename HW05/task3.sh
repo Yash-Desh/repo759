f0fe8cb6214535f73369e8836050bc4a211bd074
Yash Deshpande <23yashd@gmail.com>
2024-11-08 18:42:02 -0600

    trial bash file

--- HW05/task3.sh
+++ HW05/task3.sh
@@ -20,7 +20,7 @@ cd $SLURM_SUBMIT_DIR
 
 
 module load nvidia/cuda/11.8.0
-module load gcc/11.3.0
+module load gcc/9.4.0
 
 nvcc task3.cu vscale.cu -Xcompiler -O3 -Xcompiler -Wall -Xptxas -O3 -std=c++17 -o task3
 
