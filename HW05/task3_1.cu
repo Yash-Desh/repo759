15fbe85369cec0b0dbe884488542cd854c0fbea0
Yash Deshpande <23yashd@gmail.com>
2024-11-08 21:13:02 -0600

    task3_1 modified

--- HW05/task3_1.cu
+++ HW05/task3_1.cu
@@ -6,23 +6,33 @@
 
 int main(int argc, char *argv[])
 {
+    // threads per block
+    const int M = 512;
+
+    cudaEvent_t start;
+    cudaEvent_t stop;
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+    // Get the elapsed time in milliseconds
+    float ms;
+
     std::random_device entropy_source;
     std::mt19937_64 generator(entropy_source());
     std::uniform_real_distribution<float> dist1(-10.0, 10.0);
     std::uniform_real_distribution<float> dist2(0.0, 1.0);
 
     // n is given as command line argument
-    unsigned int n = std::atoi(argv[1]);
+    unsigned int n = std::stoi(argv[1]);
 
     // array to store n*n random float values from -10.0 to 10.0 for image
-    float *a = new float[n];
+    float *a = (float*)malloc(n*(sizeof(float)));
     if (!a)
     {
         std::cout << "Memory allocation failed for array a\n";
     }
 
     // array to store the n*n random float values from 0.0 to 1.0 for mask
-    float *b = new float[n];
+    float *b = (float*)malloc(n*(sizeof(float)));
     if (!b)
     {
         std::cout << "Memory allocation failed for array b\n";
@@ -43,29 +53,24 @@ int main(int argc, char *argv[])
     size_t size = n * sizeof(float);
 
     // allocate memory on the device (GPU)
-    cudaMalloc((void **)&d_a, size);
-    cudaMalloc((void **)&d_b, size);
+    cudaMalloc((void **)&d_a, n * sizeof(float));
+    cudaMalloc((void **)&d_b, n * sizeof(float));
 
-    cudaMemcpy(d_a, a, size, cudaMemcpyHostToDevice);
-    cudaMemcpy(d_b, b, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_a, a, n * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_b, b, n * sizeof(float), cudaMemcpyHostToDevice);
 
-    // threads per block
-    int M = 512;
+    
 
-    cudaEvent_t start;
-    cudaEvent_t stop;
-    cudaEventCreate(&start);
-    cudaEventCreate(&stop);
+   
     int number_of_block = (n + M - 1) / M;
     cudaEventRecord(start);
-    vscale<<<number_of_block, M>>>(a, b, n);
+    vscale<<<number_of_block, M>>>(d_a, d_b, n);
     cudaEventRecord(stop);
     cudaEventSynchronize(stop);
 
-    cudaMemcpy(b, d_b, size, cudaMemcpyDeviceToHost);
+    cudaMemcpy(b, d_b, n * sizeof(float), cudaMemcpyDeviceToHost);
 
-     // Get the elapsed time in milliseconds
-     float ms;
+     
      cudaEventElapsedTime(&ms, start, stop);
 
     // Durations are converted to milliseconds already thanks to std::chrono::duration_cast
@@ -79,6 +84,6 @@ int main(int argc, char *argv[])
     // deallocate memory
     cudaFree(d_a);
     cudaFree(d_b);
-    delete[] a;
-    delete[] b;
+    free(a);
+    free(b);
 }
