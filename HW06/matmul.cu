05278eb04617307225fb9f9ac4cc32335255ebeb
Yash Deshpande <23yashd@gmail.com>
2024-11-19 23:58:04 -0600

    taks-1 errors rectified

--- HW06/matmul.cu
+++ HW06/matmul.cu
@@ -21,6 +21,6 @@ void matmul(const float* A, const float* B, float* C, size_t n, unsigned int thr
     dim3 dimGrid((n + threads_per_block - 1) / threads_per_block);
     dim3 dimBlock(sqrt(threads_per_block), sqrt(threads_per_block));
 
-    matmul_kernel<<<dimGrid, dimBlock>>>(d_A, d_B, d_C, n);
+    matmul_kernel<<<dimGrid, dimBlock>>>(A, B, C, n);
 
 }
