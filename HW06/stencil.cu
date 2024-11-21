03138d9b022c389f4bd4a38c21bb2399af58fa5d
Yash Deshpande <23yashd@gmail.com>
2024-11-20 18:40:31 -0600

    stencil.cu changed

--- HW06/stencil.cu
+++ HW06/stencil.cu
@@ -15,24 +15,48 @@ __global__ void stencil_kernel(const float* image, const float* mask, float* out
     unsigned int local_thread_id = threadIdx.x;
     unsigned int global_thread_id = blockIdx.x * blockDim.x + threadIdx.x;
 
+
+    // ##############################################################################################
+
     // Load into shared memory the mask
-    if (local_thread_id < 2 * R + 1) {
-        shared_mask[local_thread_id] = mask[local_thread_id];
-    }
+    // if (local_thread_id < 2 * R + 1) {
+    //     shared_mask[local_thread_id] = mask[local_thread_id];
+    // }
 
-    // put image shared memory
-    int shared_start_idx = blockIdx.x * blockDim.x - R;
-    int shared_end_idx = shared_start_idx + blockDim.x + 2 * R;
+    // // put image shared memory
+    // int shared_start_idx = blockIdx.x * blockDim.x - R;
+    // int shared_end_idx = shared_start_idx + blockDim.x + 2 * R;
 
-    // Assume that image[i] = 1 when i < 0 or i > n − 1
-    if (shared_start_idx + local_thread_id >= 0 && shared_start_idx + local_thread_id < n) 
+    // // Assume that image[i] = 1 when i < 0 or i > n − 1
+    // if (shared_start_idx + local_thread_id >= 0 && shared_start_idx + local_thread_id < n) 
+    // {
+    //     shared_image[local_thread_id] = image[shared_start_idx + local_thread_id];
+    // } 
+    // else 
+    // {
+    //     shared_image[local_thread_id] = 1.0f; 
+    // }
+
+    if(local_thread_id < R)
     {
-        shared_image[local_thread_id] = image[shared_start_idx + local_thread_id];
-    } 
-    else 
-    {
-        shared_image[local_thread_id] = 1.0f; 
+	    int left_idx = global_thread_id - R;
+	      shared_image[threadIdx.x] = (left_idx >= 0 )?(image[left_idx]):1.0;
+	    // printf("Left halo loaded with value: %f\n", image_s[threadIdx.x]);
     }
+    if(threadIdx.x < R)
+    {
+	    int right_idx = global_thread_id + blockDim.x;
+	    shared_image[blockDim.x + R + threadIdx.x] = (right_idx < n) ? (image[right_idx]):1.0;
+	    // printf("Right halo loaded with value: %f\n",image_s[blockDim.x + R + threadIdx.x]);
+    }
+    if(global_thread_id < n)
+      shared_image[R+threadIdx.x] = image[index];
+    if(local_thread_id <= 2*R)
+      shared_image[threadIdx.x] = mask[threadIdx.x];
+
+    // ##############################################################################################
+
+
 
     // make sure all threads have completed loading into shared memory
     __syncthreads();
@@ -46,8 +70,10 @@ __global__ void stencil_kernel(const float* image, const float* mask, float* out
             outcome += shared_image[local_thread_id + R + i] * shared_mask[i+R];
         }
         shared_output[local_thread_id] = outcome;
-        output[global_thread_id] = shared_output[local_thread_id];
+        // output[global_thread_id] = shared_output[local_thread_id];
     }
+    if(global_thread_id < n) 
+      output[global_thread_id] = shared_output[threadIdx.x];
 }
 
 
